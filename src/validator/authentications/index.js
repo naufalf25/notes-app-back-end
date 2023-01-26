867d0760d43a5dd4fc010491106ec49b3fd8858d
naufalf25 <opal.farras@gmail.com>
2023-01-26 15:02:40 +0700

    Authentication implemented

---
+++ src/validator/authentications/index.js
@@ -0,0 +1,25 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const { PostAuthenticationPayloadSchema, PutAuthenticationPayloadSchema, DeteleAuthenticationPayloadSchema } = require('./schema');
+
+const AuthenticationsValidator = {
+  validatePostAuthenticationPayload: (payload) => {
+    const validationResult = PostAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validatePutAuthenticationPayload: (payload) => {
+    const validationResult = PutAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validateDeleteAuthenticationPayload: (payload) => {
+    const validationResult = DeteleAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = AuthenticationsValidator;
